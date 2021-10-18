fa76e901e7dfb35ab610d7c91b35a04d749ed64c
Stephen Camilo <stephen.camilo@live.com>
2021-10-18 00:10:53 -0300

    Add samples data to Local Storage

--- app.php
+++ app.php
@@ -37,14 +37,21 @@
 
     <div class="mt-5">
         <div v-for="item in items">
-            <div class="card">
+            <div class="card border-0">
                 <div class="card-body">
 
                     <div class="row">
                         <div class="col-1">
-                            <img class="rounded-circle w-50" src="https://pbs.twimg.com/profile_images/1254266075052683265/RFTgn0JE_400x400.jpg" />
+                            <div class="d-flex justify-content-end">
+                                <img class="rounded-circle w-50" src="https://pbs.twimg.com/profile_images/1254266075052683265/RFTgn0JE_400x400.jpg" />
+                            </div>
                         </div>
                         <div class="col-11">
+                            <div class="row">
+                                <div class="col">
+                                    <strong>[[USER FULL NAME]]</strong> @[[username]] · [[time ago]]
+                                </div>
+                            </div>
                             <div class="row">
                                 <div class="col">
                                     {{ item.text }}
@@ -72,6 +79,13 @@
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up" viewBox="0 0 16 16">
                                             <path fill-rule="evenodd" d="M3.5 6a.5.5 0 0 0-.5.5v8a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-8a.5.5 0 0 0-.5-.5h-2a.5.5 0 0 1 0-1h2A1.5 1.5 0 0 1 14 6.5v8a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-8A1.5 1.5 0 0 1 3.5 5h2a.5.5 0 0 1 0 1h-2z" />
                                             <path fill-rule="evenodd" d="M7.646.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 1.707V10.5a.5.5 0 0 1-1 0V1.707L5.354 3.854a.5.5 0 1 1-.708-.708l3-3z" />
+                                        </svg>
+                                    </span>
+                                    <span class="me-3">
+
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
                                         </svg>
                                     </span>
                                 </div>
--- js.js
+++ js.js
@@ -1,13 +1,20 @@
+var retrievedObject = localStorage.getItem('local');
+retrievedObject = JSON.parse(retrievedObject);
+if (retrievedObject === null) {
+    retrievedObject = [];
+}
+
 var app5 = new Vue({
     el: '#app',
     data: {
         item: "",
-        items: []
+        items: retrievedObject
     },
     methods: {
         addItem: function() {
             if (this.item !== '') {
                 this.items.unshift({ text: this.item });
+                localStorage.setItem('local', JSON.stringify(this.items));
             }
             this.item = '';
         }
